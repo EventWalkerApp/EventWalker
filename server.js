a7662e4687f0276e219bbe14a003d413b8259372
Sunbabaara <csunbabaara@gmail.com>
2021-08-04 00:57:49 +0000

    added new features

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+
+//home route
+app.get("/", function(req, res){
+    res.render("WELCOME")
+});
+
+
+//start server
+app.listen(5000, function(){
+    console.log("server started on port 5000");
+});
